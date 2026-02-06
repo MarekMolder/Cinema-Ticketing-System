6e090d254ea1051326c8908394e5af5f6ec555c1
maremo <maremo@taltech.ee>
2026-02-06 15:42:30 +0200

    Add security config and user provisioning from JWT

---
+++ src/main/java/com/cinema/Cinema_Ticketing/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.cinema.Cinema_Ticketing.repositories;
+
+import com.cinema.Cinema_Ticketing.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
